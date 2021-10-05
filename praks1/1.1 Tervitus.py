85d41d17f98fe87f589d857a0b6a8f13e4ccf650
Sander hera <sander.hera@khk.ee>
2021-10-05 13:38:04 +0300

    1.2 President

--- praks1/1.1 Tervitus.py
+++ praks1/1.1 Tervitus.py
@@ -1,1 +1,8 @@
-print("Tere, maailm!")
+aasta = 2016
+president = "Kersti Kaljulaid"
+lause_keskosa = ". aastal valiti Eesti presidendiks "
+
+
+
+lause = str(aasta) + lause_keskosa + president
+print(lause)
